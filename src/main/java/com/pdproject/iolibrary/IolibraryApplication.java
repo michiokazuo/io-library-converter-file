a4f9e201c5c4bea3ca0a31754e83e29e6f7d6ebc
minhduc2kvp <minhduc2kvp@gmail.com>
2020-08-17 22:33:04 +0700

    init project

---
+++ src/main/java/com/pdproject/iolibrary/IolibraryApplication.java
@@ -0,0 +1,13 @@
+package com.pdproject.iolibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IolibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IolibraryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pdproject/iolibrary/IolibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pdproject.iolibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IolibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
