71f3ec5673dcc23c66620753a6bec7d1ca82883f
michiokazuo <xuanphong10a@gmail.com>
2020-09-24 10:36:18 +0700

    google picker

--- src/main/resources/static/js/pages/ajax_page_convert_file.js
+++ src/main/resources/static/js/pages/ajax_page_convert_file.js
@@ -26,12 +26,90 @@ $(function () {
     textFileNameTo = $("#file-convert-name");
     textEmailTo = $("#email-to");
 
-    fileConvert.change(function (){
-       textFileNameFrom.val(fileConvert[0].files[0].name);
+    fileConvert.change(function () {
+        $('.result').addClass("hidden");
+        textFileNameFrom.val(fileConvert[0].files[0].name);
     });
 
     btnFileLocal.click(function () {
         fileConvert.click();
+    })
+
+    btnFileGoogleDrive.click(function () {
+        // The Browser API key obtained from the Google API Console.
+        // Replace with your own Browser API key, or your own key.
+        var developerKey = 'AIzaSyDtAzWsaO1-phtipZkgakFuVexsM6V4qU0';
+
+        // The Client ID obtained from the Google API Console. Replace with your own Client ID.
+        var clientId = "637109986363-j4jh5r54q7gdqda01pbt10lfndphki57.apps.googleusercontent.com"
+
+        // Replace with your own project number from console.developers.google.com.
+        // See "Project number" under "IAM & Admin" > "Settings"
+        var appId = "637109986363";
+
+        // Scope to use to access user's Drive items.
+        var scope = ['https://www.googleapis.com/auth/drive.file'];
+
+        var pickerApiLoaded = false;
+        var oauthToken;
+
+        loadPicker();
+
+        // Use the Google API Loader script to load the google.picker script.
+        function loadPicker() {
+            gapi.load('auth', { 'callback': onAuthApiLoad });
+            gapi.load('picker', { 'callback': onPickerApiLoad });
+        }
+
+        function onAuthApiLoad() {
+            window.gapi.auth.authorize(
+                {
+                    'client_id': clientId,
+                    'scope': scope,
+                    'immediate': false
+                },
+                handleAuthResult);
+        }
+
+        function onPickerApiLoad() {
+            pickerApiLoaded = true;
+            createPicker();
+        }
+
+        function handleAuthResult(authResult) {
+            if (authResult && !authResult.error) {
+                oauthToken = authResult.access_token;
+                createPicker();
+            }
+        }
+
+        // Create and render a Picker object for searching images.
+        function createPicker() {
+            if (pickerApiLoaded && oauthToken) {
+                var view = new google.picker.View(google.picker.ViewId.DOCS);
+                view.setMimeTypes("image/png,image/jpeg,image/jpg");
+                var picker = new google.picker.PickerBuilder()
+                    .enableFeature(google.picker.Feature.NAV_HIDDEN)
+                    .enableFeature(google.picker.Feature.MULTISELECT_ENABLED)
+                    .setAppId(appId)
+                    .setOAuthToken(oauthToken)
+                    .addView(view)
+                    .addView(new google.picker.DocsUploadView())
+                    .setDeveloperKey(developerKey)
+                    .setCallback(pickerCallback)
+                    .build();
+                picker.setVisible(true);
+            }
+        }
+
+        // A simple callback implementation.
+        function pickerCallback(data) {
+            if (data.action == google.picker.Action.PICKED) {
+                console.log(data.docs);
+                var fileId = data.docs[0].id;
+                alert('The user selected: ' + fileId);
+            }
+        }
     })
 
     showSelectOption(selectFormatFrom, listFormat);
@@ -49,6 +127,9 @@ function showSelectOption(element, list) {
 
 function chooseFormat() {
     selectFormatFrom.change(function () {
+        $('.result').addClass("hidden");
+        textFileNameFrom.val("");
+
         let new_listFormat = [];
         let valSelectFormatFrom = selectFormatFrom.val().toLowerCase();
         let valSelectFormatTo = selectFormatTo.val().toLowerCase();
@@ -66,6 +147,9 @@ function chooseFormat() {
     });
 
     selectFormatTo.change(function () {
+        textFileNameFrom.val("");
+        $('.result').addClass("hidden");
+
         let new_listFormat = [];
         let valSelectFormatFrom = selectFormatFrom.val().toLowerCase();
         let valSelectFormatTo = selectFormatTo.val().toLowerCase();
@@ -98,5 +182,15 @@ function confirmShareFile() {
 }
 
 function convertFile() {
+    btnConvert.click(function () {
+        selectFormatFrom.prop("disabled", true);
+        selectFormatTo.prop("disabled", true);
+        $(".loading").removeClass("hidden");
 
+
+        $('.loading').addClass("hidden");
+        $('.result').removeClass("hidden");
+        selectFormatFrom.prop("disabled", false);
+        selectFormatTo.prop("disabled", false);
+    })
 }
